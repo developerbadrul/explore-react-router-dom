8b8a24e495a88886c93103b1a48d99d9755965a4
Md Badrul Alam <developer.badrul@gmail.com>
2025-12-07 20:59:57 +0600

    add root & child route also add error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './routes/root'
+import { RootErrorBoundary } from './error-page'
+import Contact from './routes/Contact'
+
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello WOrld</div>
+    Component: Root,
+    ErrorBoundary: RootErrorBoundary,
+    children: [
+      {
+        path: "contacts/:contactId",
+        Component: Contact
+      },
+    ],
   }
 ])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
